daf46627540db8ffb86ec489de6e3ffe5651708e
florapetersen <floramarina@comcast.net>
2021-02-23 19:17:59 -0800

    commit tree 1acc8af0c69d9da9df4f469efe1a86e414656b2f

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,18 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map((movie) => (
+          <div>
+            <h2>Title: {movie.title}</h2>
+            <h2>Time: {movie.time}</h2>
+            <ul>
+              {movie.genres.map((genre) => (
+                <li>{genre}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
     </div>
   );
 };
